ac2f6cfc977e258b616a198a3d56021efc4ebc63
Niladri Gomes <niladri@login3.cm.cluster>
2017-04-29 20:26:42 -0700

    added source file for utils

---
+++ utils.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <stdio.h>
+#include <math.h>
+#include "utils.h"
+
+using namespace std;
+
+float f(float x)
+{
+
+	return x;
+
+}
+
+float f2(float x)
+{
+	return x*x;
+}
+
+float f_sin(float x)
+{
+	return sin(x);
+}
