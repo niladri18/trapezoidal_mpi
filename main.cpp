286c1e3f125e25d8e326bacbc2a80717739019d2
Niladri Gomes <niladri@login3.cm.cluster>
2017-04-29 19:54:08 -0700

    added the main source file

---
+++ main.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <stdio.h>
+#include "mpi.h"
+
+using namespace std;
+
+float func(float x)
+{
+	return x;
+}
+
+float trap(float a, float b, int n, float h)
+{
+	float integral;
+	float x;
+	int i;
+	x = a;	
+	integral = (func(a) + func(b) )/2.0;
+	for(i = 0; i < n-1; i++)
+	{
+		x = x + h;
+		integral = integral + func(x);	
+	}
+	return integral*h;
+}
+
+int main()
+{
+	float a = 0.0;
+	float b = 1.0;
+	int n = 1000;
+	
+	MPI_Init(NULL, NULL);
+	
+	int numproc;
+	MPI_Comm_size(MPI_COMM_WORLD, &numproc);
+
+        int rank;
+        MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+	float h = (b-a)/n;
+	int local_n = n/numproc;
+	float local_a = a + rank*local_n*h;
+	float local_b = local_a + local_n*h;
+	float integral;
+	float total;	
+	
+	integral = trap(local_a, local_b, local_n, h);
+	printf("Local a = %f, local b = %f, integral = %f  in proc %d \n", local_a, local_b, integral, rank);
+	MPI_Reduce(&integral, &total, 1, MPI_FLOAT, MPI_SUM, 0, MPI_COMM_WORLD);
+	if (rank == 0)
+	{
+		printf("Answer is %f \n", total);
+		
+	}
+
+
+	return 0;
+
+}
